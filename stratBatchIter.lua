c4a83113f7bae64f04ae5172322ea0aabe23c71d
Nils Hammerla <nils@hulk.ncl.ac.uk>
2015-05-12 14:48:36 +0100

    v0.2

--- stratBatchIter.lua
+++ stratBatchIter.lua
@@ -1,26 +1,38 @@
 function stratBatchIter(targets, approxBatchsize)
-	local T = targets:clone()+torch.randn(target:size())*0.001 -- add some jitter for randomness
+	local T = targets:clone()+torch.randn(targets:size())*0.001 -- add some jitter for randomness
 	local nbatch = torch.floor(T:size(1) / approxBatchsize)
-	
-	-- these will be 1,2,3,4,..,nbatch,1,2,3,4,...,nbatch,...
-	local batchIds = torch.range(1,T:size(1)):viewAs(T)
-	for i=1,batchIds:size(1),
-		batchIds[i] = 1 + batchIds[i] % nbatch 
-	end
-	
+    local batchIds = torch.zeros(T:size())
+
 	-- index of sorted elements 
 	local _, ind = torch.sort(T)
+
+    -- prepare batch table
+    batches = {}
+    for i=1,nbatch do
+        batches[i] = {}
+    end
+
 	
-	-- re-assign to batch
-	batchIds:index(1,ind:long()) = batchIds
-	
+	-- assign a batch-id to each sample
+    for i=1,ind:size(1) do
+        -- assign elements in ascending order to batches
+        -- ind[1] -> 1
+        -- ind[2] -> 2 
+        -- ...
+        -- ind[nbatch]   -> nbatch
+        -- ind[nbatch+1] -> 1
+        -- ...
+        table.insert(batches[1 + (i-1) % nbatch], ind[i])
+        --batchIds[ind[i]] = 1 + (i-1) % nbatch
+    end
+
 	-- return iterator, which yields a bytetensor for addressing data
 	local batchIndex = 0
 	return function()
 		batchIndex = batchIndex + 1
-		if batchIndex == nbatch then
-			batchIndex = 1
-		end
-		return torch.eq(batchIds, batchIndex)
+		if batchIndex <= nbatch then
+            return torch.LongTensor(batches[batchIndex])
+		    --return torch.eq(batchIds, batchIndex)
+        end
 	end
 end
