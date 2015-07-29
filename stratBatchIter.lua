168407bb9a8a58690a6f3698bc8bbec0c489aaca
Nils Hammerla <nils.hammerla@ncl.ac.uk>
2015-07-29 09:30:00 +0100

    update

--- stratBatchIter.lua
+++ stratBatchIter.lua
@@ -1,31 +1,37 @@
 function stratBatchIter(targets, approxBatchsize)
-    -- clone the targets and add some jitter
-	local T = targets:clone()+torch.randn(targets:size())*0.001
-    -- we use floor here, batches will be a bit bigger than requested
+	local T = targets:clone()+torch.randn(targets:size()):typeAs(targets)*0.001 -- add some jitter for randomness
 	local nbatch = torch.floor(T:size(1) / approxBatchsize)
+	local batchIds = torch.zeros(T:size())
 
-	-- indeces of sorted elements 
+	-- index of sorted elements
 	local _, ind = torch.sort(T)
 
-    -- prepare table for batches
-    batches = {}
-    for i=1,nbatch do
-        batches[i] = {}
-    end
+	-- prepare batch table
+	batches = {}
+	for i=1,nbatch do
+	    batches[i] = {}
+	end
+
 
 	-- assign a batch-id to each sample
-    for i=1,ind:size(1) do
-        -- assign elements in ascending order to batches
-        table.insert(batches[1 + (i-1) % nbatch], ind[i])
-    end
+	for i=1,ind:size(1) do
+		-- assign elements in ascending order to batches
+		-- ind[1] -> 1
+		-- ind[2] -> 2
+		-- ...
+		-- ind[nbatch]   -> nbatch
+		-- ind[nbatch+1] -> 1
+		-- ...
+		table.insert(batches[1 + (i-1) % nbatch], ind[i])
+	end
 
-	-- return iterator, which yields a list of indeces for each batch
+	-- return iterator, which yields a bytetensor for addressing data
 	local batchIndex = 0
 	return function()
 		batchIndex = batchIndex + 1
 		if batchIndex <= nbatch then
-            return torch.LongTensor(batches[batchIndex])
-		    --return torch.eq(batchIds, batchIndex)
-        end
+			-- the return tensor can be used for addressing
+			return torch.LongTensor(batches[batchIndex])
+		end
 	end
 end
